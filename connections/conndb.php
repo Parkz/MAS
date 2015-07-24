5ea0ee6e79b6a4e0b6c93d1ecffa225af08873df
Parkz <Parkz89@gmail.com>
2015-07-24 17:01:46 -0400

    Update conndb.php

--- connections/conndb.php
+++ connections/conndb.php
@@ -2,8 +2,8 @@
 # Type="MYSQL"
 # HTTP="true"
 $hostname_conndb = "localhost";
-$database_conndb = "csmwsu_mas";
-$username_conndb = "csmwsu_mas";
-$password_conndb = "massql12";
+$database_conndb = "";
+$username_conndb = "";
+$password_conndb = "";
 $conndb = mysql_pconnect($hostname_conndb, $username_conndb, $password_conndb) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
